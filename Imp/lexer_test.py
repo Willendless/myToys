fd2f8ad704bd6fef7413224d51738c64bfa1b1db
Jiarui Li <jiaruili@berkeley.edu>
2020-06-08 18:56:37 -0700

    refactor: update project directory

--- Imp/lexer_test.py
+++ Imp/lexer_test.py
@@ -1,19 +1,17 @@
 '''
 @Author: Willendless
-@Date: 2020-06-08 15:14:04
-@Description: test for lexer
-@FilePath: \Imp\imp\tests\lexer_test.py
+@Date: 2020-06-08 18:24:55
+@Description: Test the functionality of lexer
+@FilePath: \Imp\lexer_test.py
 '''
-
+import imp.lexer
 import sys
-
-from .. import lexer
 
 if __name__ == '__main__':
     filename = sys.argv[1]
     file = open(filename)
     text = file.read()
     file.close()
-    tokens = lexer.imp_lexer(text)
+    tokens = imp.lexer.imp_lexer(text)
     for token in tokens:
         print(token)
